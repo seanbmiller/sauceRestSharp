a9edd695d91a7506b9bf0412cba05ddf5390caf6
Sean Miller <seanbmiller@example.com>
2013-08-18 00:35:57 -0400

    Adding files

---
+++ SauceRest-CS/SauceREST.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace SauceRESTClient
+{
+    public class SauceREST
+    {
+        protected String username;
+        protected String accessKey;
+
+        public static String RESTURL = "http://saucelabs.com/rest";
+
+        public SauceREST(String username, String accessKey)
+        {
+            this.username = username;
+            this.accessKey = accessKey;
+        }
+
+        public void jobPassed(String jobId)
+        {
+            var updates = new Dictionary<String, Object>();
+            updates.Add("passed", true);
+            updateJobInfo(jobId, updates);
+        }
+
+        public void jobFailed(String jobId)
+        {
+            var updates = new Dictionary<String, Object>();
+            updates.Add("passed", false);
+            updateJobInfo(jobId, updates);
+        }
+
+        public void updateJobInfo(String jobId, Dictionary<String, Object> updates)
+        {
+            String result = null;
+            Uri restEndpoint = new Uri(RESTURL + "/v1/" + username + "/jobs/" + jobId);
+            String credentials = Convert.ToBase64String(ASCIIEncoding.ASCII.GetBytes(username+":"+accessKey));
+            String json = JsonConvert.SerializeObject(updates);            
+            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(restEndpoint);
+            request.Method = "PUT";
+            request.Accept = "application/json";
+            request.Headers.Add("Authorization", "Basic " + credentials);
+            using (var writer = new StreamWriter(request.GetRequestStream()))
+            {
+                writer.Write(json);
+                writer.Close();
+                var response = (HttpWebResponse)request.GetResponse();
+                using (var streamReader = new StreamReader(response.GetResponseStream()))
+                {
+                    result = streamReader.ReadToEnd();  
+                }
+            }
+        }
+    }
+}
---
+++ SauceRestTest/Test.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SauceRESTClient;
+
+namespace SauceRestTest
+{
+    [TestClass]
+    public class Test
+    {
+        private string username = "";
+        private string accessKey = "";
+        private string testId = "";
+
+        [TestMethod]
+        public void Pass()
+        {
+            var sauce = new SauceREST(username,accessKey);
+            sauce.jobPassed(testId);
+        }
+    }
+}
